07c7a7f8fd2d616196ea6f9b053d73ecfc05ac80
Cyan <a484599279@gmail.com>
2019-02-12 18:41:33 +0800

    创建jpegHelper类

---
+++ HamOCR/CyImg/jpegHelper/jpegHelper.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <jpeglib.h>
+#include <setjmp.h>
+class jpegHelper
+{
+public:
+	jpegHelper();
+	static void read_JPEG_file(
+		const char * filename,
+		unsigned char ** img_buff,
+		unsigned int & output_width,
+		unsigned int & output_height,
+		unsigned int & output_components);
+	~jpegHelper();
+private:
+	struct my_error_mgr {
+		struct jpeg_error_mgr pub;
+
+		jmp_buf setjmp_buffer;
+	};
+
+	typedef struct my_error_mgr * my_error_ptr;
+
+	METHODDEF(void) my_error_exit(j_common_ptr cinfo)
+	{
+		/* cinfo->err really points to a my_error_mgr struct, so coerce pointer */
+		my_error_ptr myerr = (my_error_ptr)cinfo->err;
+
+		/* Always display the message. */
+		/* We could postpone this until after returning, if we chose. */
+		(*cinfo->err->output_message) (cinfo);
+
+		/* Return control to the setjmp point */
+		longjmp(myerr->setjmp_buffer, 1);
+	}
+};
+
