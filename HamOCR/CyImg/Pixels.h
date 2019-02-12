565dec0b82048480a8c5f8c64b93088a59ff1e7b
Cyan <a484599279@gmail.com>
2019-02-12 21:44:22 +0800

    实现Pixels::GetPixels、Pixels::GetRaw;//获取元数据接口

--- HamOCR/CyImg/Pixels.h
+++ HamOCR/CyImg/Pixels.h
@@ -11,6 +11,8 @@ public:
 	void LoadFromJpegFile(const string & filename);
 	void LoadFromJpegBytes(unsigned char bytes[], size_t length);
 	void SaveAsJpeg(const string & filename);
+	const vector<Pixel> & GetPixels() { return raw; }
+	const unsigned char *GetRaw() { return raw_bytes; }
 	~Pixels();
 private:
 	vector<Pixel> raw;
