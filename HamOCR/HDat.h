1d48dc3db9d2ca62028de4e49f1244943a020136
Cyan <a484599279@gmail.com>
2019-02-14 10:53:27 +0800

    为HDat增加引用计数特性

--- HamOCR/HDat.h
+++ HamOCR/HDat.h
@@ -6,7 +6,39 @@
 using std::string;
 struct HDat
 {
+	HDat() :use_count(new unsigned int(1)) {}
+	HDat(const HDat& h)
+	{
+		++*h.use_count;
+		this->data = h.data;
+		this->length = h.length;
+		this->str = h.str;
+		this->use_count = h.use_count;
+	}
+	HDat & operator=(const HDat &h)
+	{
+		++*h.use_count;
+		if (--*use_count == 0)
+		{
+			delete[] data;
+			delete use_count;
+		}
+		this->data = h.data;
+		this->length = h.length;
+		this->str = h.str;
+		this->use_count = h.use_count;
+	}
 	string str = "";
 	unsigned int length;
 	unsigned char *data;
+	~HDat() 
+	{ 
+		if (--*use_count == 0)
+		{
+			delete[] data;
+			delete use_count;
+		}
+	}
+private:
+	unsigned int *use_count;
 };
