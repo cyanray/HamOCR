01ce8faf6329aa045d487696c2345a9ebb9361aa
Cyan <a484599279@gmail.com>
2019-02-14 10:47:46 +0800

    实现HDatHelper::ReadOne;(有缺陷，未测试)

--- HamOCR/HDatHelper.cpp
+++ HamOCR/HDatHelper.cpp
@@ -6,7 +6,7 @@ void HDatHelper::Open(const string & filename)
 	if (fp == nullptr) throw std::exception("Open file failed.");
 	char buff[8]{ 0 };
 	char t = fread(buff, 8, 1, fp);
-	if (strcmp(buff,"HamOCR\n"))
+	if (strcmp(buff, "HamOCR\n"))
 	{
 		throw std::exception("Not a HDat file.");
 	}
@@ -22,7 +22,23 @@ void HDatHelper::Create(const string & filename)
 
 HDat HDatHelper::ReadOne()
 {
-	return HDat();
+	HDat t;
+	char str_buff[33];//规定str最多32个字符
+	for (size_t i = 0; true; ++i)
+	{
+		if (feof(fp)) throw std::exception("HDat file format is incorrect.");
+		fread(str_buff + i, sizeof(char), 1, fp);
+		if (str_buff[i] == '\0') break;
+	}
+	t.str = string(str_buff);
+	//接下来读取"图片标识"的长度
+	unsigned int len_buf = 0;
+	fread(&len_buf, sizeof(unsigned int), 1, fp);
+	t.length = len_buf;
+	//接下来读取“图片标识”
+	unsigned char *data_buff = new unsigned char[len_buf];
+	fread(data_buff, len_buf, 1, fp);
+	return t;
 }
 
 void HDatHelper::AppendOne(HDat & hdat)
